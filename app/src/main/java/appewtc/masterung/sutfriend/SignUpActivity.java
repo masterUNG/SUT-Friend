b8bc2505edffdf099bf9b3ecf1e37a9a49f4ff35
masterUNG <phrombutr@gmail.com>
2016-09-21 11:16:22 +0700

    Explicit String

--- app/src/main/java/appewtc/masterung/sutfriend/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/sutfriend/SignUpActivity.java
@@ -1,17 +1,35 @@
 package appewtc.masterung.sutfriend;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    //Explicit การประกาศตัวแปร
+    private EditText nameEditText, addressEditText,
+            phoneEditText, userEditText, passwordEditText;
+    private String nameString, addressString, phoneString,
+            userString, passwordString, genderString, imageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        addressEditText = (EditText) findViewById(R.id.editText2);
+        phoneEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
     }   // Main Method
 
 
-
 }   // Main Class
+
+
+
+
+
