d305f0c8d0ae0a330ffd9fe884b5d53754f52a00
masterUNG <phrombutr@gmail.com>
2016-09-23 13:20:31 +0700

    Last

--- app/src/main/java/appewtc/masterung/sutfriend/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/sutfriend/ServiceActivity.java
@@ -1,13 +1,37 @@
 package appewtc.masterung.sutfriend;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView textView;
+    private ListView listView;
+    private String loginString;
+    private String[] nameStrings, imageStrings, genderStrings,
+            addressStrings, phoneStrings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+        //Bind Widget
+        textView = (TextView) findViewById(R.id.textView8);
+        listView = (ListView) findViewById(R.id.listView);
+
+        //Get Value From Intent
+        loginString = getIntent().getStringExtra("Login");
+        nameStrings = getIntent().getStringArrayExtra("Name");
+        imageStrings = getIntent().getStringArrayExtra("Image");
+        genderStrings = getIntent().getStringArrayExtra("Gender");
+        addressStrings = getIntent().getStringArrayExtra("Address");
+        phoneStrings = getIntent().getStringArrayExtra("Phone");
+
+    }   // Main Method
+
+}   // Main Class
