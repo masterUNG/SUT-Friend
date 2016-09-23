30ef9dd7663d75fff280003bbbc1e67278081270
masterUNG <phrombutr@gmail.com>
2016-09-23 10:22:53 +0700

    Read JSON

--- app/src/main/java/appewtc/masterung/sutfriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/sutfriend/MainActivity.java
@@ -1,10 +1,17 @@
 package appewtc.masterung.sutfriend;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,45 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
 
+    private class SynchronizeData extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private Context context;
+        private static final String urlJSON = "http://swiftcodingthai.com/Sut/get_data_master.php";
+
+        public SynchronizeData(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("SutFriendV3", "e doInBack ==> " + e.toString());
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("SutFriendV3", "JSON ==> " + s);
+
+
+        }   // onPost
+
+    }   //SynData Class
+
     public void clickSignIn(View view) {
 
         //Get Value from Edit Text
@@ -39,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            SynchronizeData synchronizeData = new SynchronizeData(this);
+            synchronizeData.execute();
 
         }
 
