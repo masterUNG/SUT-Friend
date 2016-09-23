3c564005b3f5865a51cc062c126eed555b617c92
masterUNG <phrombutr@gmail.com>
2016-09-23 09:43:58 +0700

    Check Space finish

--- app/src/main/java/appewtc/masterung/sutfriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/sutfriend/MainActivity.java
@@ -4,14 +4,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passwordEditText = (EditText) findViewById(R.id.editText7);
+
     }   // Main Method
+
+    public void clickSignIn(View view) {
+
+        //Get Value from Edit Text
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert(this, R.drawable.doremon48,
+                    "Have Space", "Please Fill All Every Blank");
+            myAlert.myDialog();
+
+        } else {
+            //No Space
+
+        }
+
+
+    }   // clickSignIn
 
     //Get Event From Click Button
     public void clickSignUpMain(View view) {
